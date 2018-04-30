44e91d4e0b578786797757e920a40e576481abd1
mikaelbeat <ryynanenphm@gmail.com>
2018-04-30 14:42:11 +0300

    Update

---
+++ REST_API_Framework/src/test/java/training/constants/Auth.java
@@ -0,0 +1,10 @@
+package training.constants;
+
+public class Auth {
+
+	public static final String CONSUMER_KEY = "fzDfnjmtGavB1LnB5bWvxMgvH";
+	public static final String CONSUMER_SECRET_KEY = "LW8wKiFws6GunWMsuTmcM7Yqw9CNIKzCvybYLt010OJRolBYZT";
+	public static final String ACCESS_TOKEN = "988679202609291264-foMMhGRXlmuNsfHCaAl9RT5zQFeYpTk";
+	public static final String ACCESS_SECRET_TOKEN = "n2hgWDPQCF1jZAO1u31jXjRirdpwfN1Jteg6DIlIdWSGN";
+	
+}
---
+++ REST_API_Framework/src/test/java/training/constants/Path.java
@@ -0,0 +1,8 @@
+package training.constants;
+
+public class Path {
+
+	public static final String BASE_URI = "https://api.twitter.com";
+	public static final String BASE_PATH = "/1.1/statuses";
+	
+}
