675fa5b956d4b2326544284b9a3aae31f8aa93ef
Ryynänen,Petri <petri.ryynanen@accenture.com>
2018-05-03 10:07:12 +0300

    Update

--- REST_API_Framework/src/test/java/training/constants/Endpoints.java
+++ REST_API_Framework/src/test/java/training/constants/Endpoints.java
@@ -5,6 +5,6 @@ public class Endpoints {
 	public static final String STATUSES_USER_TIMELINE = "/user_timeline.json";
 	public static final String STATUSES_TWEET_POST = "/update.json";
 	public static final String STATUSES_TWEET_READ_SINGLE = "/show.json";
-	public static final String STATUSES_TWEET_DESTROY = "/destroy.json";
+	public static final String STATUSES_TWEET_DESTROY = "/destroy{id}.json";
 	
 }
